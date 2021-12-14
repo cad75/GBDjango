c230c44ef114b1b1686d4ae2986f47b38e74b37d
cad <cad75@yandex.ru>
2021-12-14 20:44:58 +0400

    Django2_Lesson_6

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -92,20 +92,20 @@ AUTH_USER_MODEL = 'authapp.ShopUser'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     },
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'geekshop',
-        'USER': 'postgres'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     },
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'geekshop',
+#         'USER': 'postgres'
+#     },
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -148,6 +148,14 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
+
+# ENV_TYPE = os.getenv('ENV_TYPE')
+# if ENV_TYPE == 'local':
+#     STATICFILES_DIRS = (
+#         BASE_DIR / 'static',
+#     )
+# else:
+#     STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 
